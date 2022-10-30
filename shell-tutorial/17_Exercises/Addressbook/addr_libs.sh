7eb75ecb47c4a1143729187ce4f20e71c28ffa5b
YS <yoonsam86@gmail.com>
2022-10-30 15:46:30 +0900

    17. Exercises Addressbook library - remove book

--- shell-tutorial/17_Exercises/Addressbook/addr_libs.sh
+++ shell-tutorial/17_Exercises/Addressbook/addr_libs.sh
@@ -2,6 +2,18 @@
 BOOK=~/.addressbook
 export BOOK
 
+confirm()
+{
+    echo -en "$@"
+    read ans
+    ans=`echo $ans | tr '[a-z]' '[A-Z]'`
+    if [ "$ans" == "Y" ]; then
+        return 0
+    else 
+        return 1
+    fi
+}
+
 num_lines()
 {
     grep -i "$@" $BOOK|wc -l| awk '{ print $1 }'
@@ -59,12 +71,49 @@ add_item()
     echo "${name}:${phone}:${email}" >> $BOOK
 }
 
-edit_item() 
+locate_single_item()
 {
-    echo "called by edit_item"
+    echo -en "Item to search for: "
+    read search
+    n=`num_lines "$search"`
+    if [ -z "$n" ]; then
+        n=0
+    fi
+    while [ "${n}" -ne "1" ]; do
+        #list_items "$search"
+        echo -en "${n} matches found. Please choose a "
+        case "$n" in
+            "0") echo "less" ;;
+            "*") echo "more" ;;
+        esac
+        echo "specific search term (q to return to main): "
+        read search
+        if [ "$search" == "q" ]; then
+            return 0
+        fi
+        n=`num_lines "$search"`
+    done
+    return `grep -in $search $BOOK |cut -d":" -f1`
 }
 
 remove_item() 
 {
-    echo "called by remove_item"
+    locate_single_item
+    # tail -n : 끝에서 n줄, tr A B : A 를 B로 치환
+    search=`head -$? $BOOK | tail -1 | tr ' ' ','`
+    if [ -z "{$search}" ]; then
+        return
+    fi
+    list_items "$search"
+    confirm "Remove?"
+    if [ "$?" -eq "0" ]; then 
+        grep -v "$search" $BOOK > ${BOOK}.tmp ; mv ${BOOK}.tmp ${BOOK}
+    else 
+        echo "NOT REMOVING"
+    fi
+}
+
+edit_item() 
+{
+    echo "called by edit_item"
 }
