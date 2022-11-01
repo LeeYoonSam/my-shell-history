4a619b3472c106428851441159e19488eda753b0
LeeYoonSam <yoonsam86@gmail.com>
2022-11-02 08:48:32 +0900

    17. Exercises - Dictionary Traverse2

--- shell-tutorial/17_Exercises/DictionaryTraverse/traverse2.sh
+++ shell-tutorial/17_Exercises/DictionaryTraverse/traverse2.sh
@@ -2,6 +2,8 @@
 
 doindent()
 {
+    # 트리의 깊이에 따라 들여쓰기를 하십시오. 
+    # 환경에 따라 echo -en " " 대신 echo " \c"를 사용해야 할 수도 있습니다.
     j=0;
     while [ "$j" -lt "$1" ]; do
         echo "  \c"
@@ -9,26 +11,26 @@ doindent()
     done
 }
 
-traverse() 
+traverse()
 {
     # Traverse a directory
-    cd "$1"
+    indent="$2"
 
-    ls | while read i
+    ls "$1" | while read i
     do
         doindent $2
-        if [ -d "$i" ]; then
-            echo "Directory: $i"
+        if [ -d "$1/$i" ]; then
+            echo "Directory: $1/$i"
             # 이것을 서브쉘로 호출한다는 것은 호출된 함수가 디렉토리를 변경할 때 현재 작업 디렉토리에 영향을 미치지 않는다는 것을 의미합니다.
-            (traverse "$i" `expr $2 + 1`)
+            traverse "$1/$i" `expr $2 + 1`
         else
-            echo "File: $i"
+            echo "File: $1/$i"
         fi
     done
 }
 
 if [ -z "$1" ]; then
     traverse . 0
-else
+else 
     traverse "$1" 0
 fi
