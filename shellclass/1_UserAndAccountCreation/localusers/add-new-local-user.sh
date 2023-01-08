5661b610964ca3705175bbd69550abeef15d6e79
YS <yoonsam86@gmail.com>
2023-01-08 11:06:09 +0900

    Exercise3 - add-new-local-user 수도코드 추가

---
+++ shellclass/1_UserAndAccountCreation/localusers/add-new-local-user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make sure the script is being executed with superuser privileges.
+
+# If the user doesn't supply at least one argument, then give them help.
+
+# The first parameter is the user name.
+
+# The rest of the parameters are for the account comments.
+
+# Generate a password.
+
+# Create the user with the password.
+
+# Check to see if the useradd command succeeded.
+
+# Set the password.
+
+# Check to see if the passwd command succeeded.
+
+# Force password change on first login.
+
+# Display the username, password, and the host where the user was created.
