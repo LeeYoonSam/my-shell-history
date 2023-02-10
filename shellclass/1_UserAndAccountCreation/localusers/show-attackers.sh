6ccb2903acc8a4f5e09316ddabeb104013e42ef0
YS <yoonsam86@gmail.com>
2023-02-10 18:12:33 +0900

    shellcalss - update Parsing Log Files - Exercise 6

--- shellclass/1_UserAndAccountCreation/localusers/show-attackers.sh
+++ shellclass/1_UserAndAccountCreation/localusers/show-attackers.sh
@@ -20,12 +20,12 @@ echo 'Count,IP,Location'
 # Loop through the list of failed attempts and corresponding IP addresses.
 grep Failed ${LOG_FILE} | awk '{ print $(NF - 3) }' | sort | uniq -c | sort -nr | while read COUNT IP
 do
- # If the number of failed attempts is greater than the limit, display count, IP, and location.
-if [[ "${COUNT}" -gt ${LIMIT} ]]
-then
-    LOCATION=$(geoiplookup ${IP} | awk -F ', ' '{print $2}')
-    echo "${COUNT},${IP},${LOCATION}"
-fi
+    # If the number of failed attempts is greater than the limit, display count, IP, and location.
+    if [[ "${COUNT}" -gt ${LIMIT} ]]
+    then
+        LOCATION=$(geoiplookup ${IP} | awk -F ', ' '{print $2}')
+        echo "${COUNT},${IP},${LOCATION}"
+    fi
 done
 
 exit 0
