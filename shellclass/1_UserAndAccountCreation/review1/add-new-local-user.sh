c6a43cbdf6952f79e1d14c7da9593977f3c5dcfc
YS <yoonsam86@gmail.com>
2023-03-02 17:09:56 +0900

    shellclass - add new local user

---
+++ shellclass/1_UserAndAccountCreation/review1/add-new-local-user.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Make sure the script is being executed with superuser privileges.
+if [[ "${UID}" -ne 0 ]]
+then
+  echo 'Please run with sudo or as root.'
+  exit 1
+fi
+
+# If the user doesn't supply at least one argument, then give them help.
+if [[ "${#}" -lt 1 ]]
+then
+  echo 'Useage: ${0} USER_NAME [COMMENT]...'
+  echo 'Cretae an account on the local system with the name of USER_NAME and a comments field of COMMENT.'
+  exit 1
+fi
+
+
+# The first parameter is the user name.
+USER_NAME="${1}"
+
+# The rest of the parameters are for the account comments.
+shift
+COMMENT="${@}"
+
+# Generate a password.
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+
+# Create the user with the password.
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Check to see if the useradd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The account could not be created.'
+  exit 1
+fi
+
+# Set the password.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The password for the account could not be set.'
+  exit 1
+fi
+
+
+# Force password change on first login.
+passwd -e ${USER_NAME}
+
+# Display the username, password, and the host where the user was created.
+echo
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
+echo
+echo 'hostname:'
+echo "${HOSTNAME}"
+exit 0
