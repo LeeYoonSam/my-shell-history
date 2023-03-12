771e72b48b27695db9ec3a9a1d0c93ce58a592df
YS <yoonsam86@gmail.com>
2023-03-12 14:27:08 +0900

    shellclass - added pseudo code for run-everywhere.sh and show-attackers.sh

---
+++ shellclass/exercises/run-everywhere-pseudo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# A list of servers, one per line.
+
+# Options for the ssh command.
+
+usage() {
+  # Display the usage and exit.
+  exit 1
+}
+
+# Make sure the script is not being executed with superuser privileges.
+
+# Parse the options.
+
+# Remove the options while leaving the remaining arguments.
+
+# If the user doesn't supply at least one argument, give them help.
+
+# Anything that remains on the command line is to be treated as a single command.
+
+# Make sure the SERVER_LIST file exists.
+
+# Expect the best, prepare for the worst.
+
+# Loop through the SERVER_LIST
---
+++ shellclass/exercises/show-attackers-pseudo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Count the number of failed logins by IP address.
+# If there are any IPs with over LIMIT failures, display the count, IP, and location.
+
+
+# Make sure a file was supplied as an argument.
+
+# Display the CSV header.
+
+# Loop through the list of failed attempts and corresponding IP addresses.
