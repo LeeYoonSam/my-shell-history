cc5835b5248f12d4ce673c025b50cd8378c5b034
YS <yoonsam86@gmail.com>
2023-03-24 17:30:28 +0900

    gpt-script - rename 하는 로직 수정

--- RealSample/gpt-script/rename-file-remove-word.sh
+++ RealSample/gpt-script/rename-file-remove-word.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# # 디렉토리 경로가 인자로 제공되었는지 확인합니다.
+# 디렉토리 경로가 인자로 제공되었는지 확인합니다.
 if [[ $# -eq 0 ]]; then
     echo "Usage: ${0} <directory_path>" >&2
     exit 1
@@ -16,20 +16,20 @@ read -a exclude_words
 cd "$dir" || exit
 for file in *.*; do
     # file 이 일반적인 파일인지 확인(디렉토리는 안됨), 확장자 없음
-    if [[ -f "$file" && ${file##*.} == $file ]]; then
+    if [[ -f "$file" ]]; then
+        # 확장자없이 파일명 가져오기
+        new_name=$(basename "$file" | sed 's/\.[^\.]*$//')
         # 파일 이름에서 제외시킬 단어 제거
-        new_name="$file"
         for word in "${exclude_words[@]}"; do
             # ${new_name//$word/} 구문을 사용하여 파일 이름에서 제외 단어를 제거
-            new_name=${new_name//$word/}
+            new_name=$(echo "$new_name" | sed "s/$word//g")
         done
 
-        # sed 명령을 사용하여 파일 이름의 시작과 끝에 있는 공백과 특수 문자를 제거
-        new_name=$(echo "$new_name" | sed 's/^[^a-zA-Z0-9]*//;s/[^a-zA-Z0-9]*$//')
+        new_name=$(echo "$new_name" | sed -E 's/^ +| +$//g;')
 
         # 새로운 파일 이름과 기존 파일 이름이 같다면 기존 파일 제거
         if [[ "$file" != "$new_name" ]]; then
-            mv "$file" "$new_name"
+            mv "$file" "$new_name.${file##*.}"
             echo "Renamed '$file' to '$new_name'"
         fi
     fi
