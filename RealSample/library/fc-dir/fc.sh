4974509bf84c4a5d340dea102db723e91da85da2
YS <yoonsam86@gmail.com>
2023-07-21 09:15:38 +0900

    fc - Add the help function

--- RealSample/library/fc-dir/fc.sh
+++ RealSample/library/fc-dir/fc.sh
@@ -6,11 +6,23 @@ function fc {
     local PREVIEW="ls -alh"
 
     function __fc_help() {
+        printf "
+[ fc ] - favorite command collection
 
+usage: fc [add | remove | list | clean | find | help]
+short: fc [a   | rm     | l    | c     | f    | help]
+
+Examples
+fc add     => Add command to fc index file.
+fc remove  => Remove selected command in index file.
+fc list    => Print index file.
+fc clean   => Remove invalid directories in index file.
+fc help    => Show help.
+\n"
     }
 
     function __fc_main() {
-
+        echo "welcom to fc"
     }
 
     function __fc_get_list() {
@@ -19,3 +31,6 @@ function fc {
 
     __fc_main $@
 }
+
+# todo: remove
+fc
